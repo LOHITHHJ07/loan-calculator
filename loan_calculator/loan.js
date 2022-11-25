32fc6f9e103d5dee1945af6932ccedd558b2c788
LOHITH H J <95174525+LOHITHHJ07@users.noreply.github.com>
2022-11-25 12:10:03 +0530

    Add files via upload

---
+++ loan_calculator/loan.js
@@ -0,0 +1,56 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+    document.getElementById("result").style.display = "none";
+    document.getElementById("loading").style.display = "block";
+    setTimeout(calculate, 2000);
+    e.preventDefault();
+  });
+  
+  function calculate(e) {
+    const amount = document.getElementById("amount");
+    const interest = document.getElementById("interest");
+    const years = document.getElementById("years");
+    const monthlyPayment = document.getElementById("monthly");
+    const totalAmount = document.getElementById("total-amount");
+    const totalInterest = document.getElementById("total-interest");
+  
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+  
+    if (isFinite(monthly)) {
+      monthlyPayment.value = monthly.toFixed(2);
+      totalAmount.value = (monthly * calculatedPayments).toFixed(2);
+      totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  
+      document.getElementById("result").style.display = "block";
+      document.getElementById("loading").style.display = "none";
+    } else {
+      showAlert("Please enter the amounts");
+    }
+    e.preventDefault();
+  }
+  
+  function showAlert(error) {
+    const errorDiv = document.createElement("div");
+  
+    errorDiv.className = "alert alert-danger";
+    errorDiv.appendChild(document.createTextNode(error));
+    const card = document.querySelector(".card");
+    const heading = document.querySelector(".heading");
+  
+    card.insertBefore(errorDiv, heading);
+  
+    setTimeout(function () {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+  
+
+// document.getElementById('form').addEventListener("submit",hello);
+//  function hello(e)
+//  {
+//     console.log("hello")
+//     e.preventDefault();
+//  }
